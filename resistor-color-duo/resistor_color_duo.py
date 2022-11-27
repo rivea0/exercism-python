a10a5159c7b19b7623a86a097967900a7c1c42dc
rivea0 <58330360+rivea0@users.noreply.github.com>
2022-11-28 01:14:50 +0300

    Add resistor-color-duo

---
+++ resistor-color-duo/resistor_color_duo.py
@@ -0,0 +1,15 @@
+def value(colors):
+    COLOR_VALUES = {
+        'black': 0,
+        'brown': 1,
+        'red': 2,
+        'orange': 3,
+        'yellow': 4,
+        'green': 5,
+        'blue': 6,
+        'violet': 7,
+        'grey': 8,
+        'white': 9,        
+    }
+
+    return int(str(COLOR_VALUES[colors[0]]) + str(COLOR_VALUES[colors[1]]))
