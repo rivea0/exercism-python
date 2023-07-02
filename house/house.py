a749ce78d6f358626b7e9ed81c7c058a3e90032c
rivea0 <58330360+rivea0@users.noreply.github.com>
2023-07-02 03:43:32 +0300

    Add house

--- house/house.py
+++ house/house.py
@@ -1,40 +1,23 @@
 def recite(start_verse, end_verse):
-    things = [
-        'the horse and the hound and the horn',
-        'the farmer sowing his corn',
-        'the rooster that crowed in the morn',
-        'the priest all shaven and shorn', 
-        'the man all tattered and torn', 
-        'the maiden all forlorn', 
-        'the cow with the crumpled horn', 
-        'the dog', 
-        'the cat', 
-        'the rat', 
-        'the malt', 
-        'the house that Jack built'
-    ]
+    verses = {
+        2: 'the malt that lay',
+        3: 'the rat that ate',
+        4: 'the cat that killed',
+        5: 'the dog that worried',
+        6: 'the cow with the crumpled horn that tossed',
+        7: 'the maiden all forlorn that milked',
+        8: 'the man all tattered and torn that kissed',
+        9: 'the priest all shaven and shorn that married',
+        10: 'the rooster that crowed in the morn that woke',
+        11: 'the farmer sowing his corn that kept',
+        12: 'the horse and the hound and the horn that belonged to'
+    }
 
-    end = [
-        'belonged to',
-        'kept',
-        'woke',
-        'married',
-        'kissed',
-        'milked',
-        'tossed',
-        'worried',
-        'killed',
-        'ate', 
-        'lay in'
-    ]
-
-    str = f'This is {things[len(things) - end_verse]}'
-
-    for i in range(end_verse - 1):
-        str += f' that {end[(len(end) - end_verse) + i + 1]} {things[(len(things) - end_verse) + i + 1]}'
-    # for i, v in enumerate(things):
-    #     str += f'that {end[end_verse - (i + 1)]} {things[end_verse]} '
-    # return f'This is {things[end_verse - 1]} that {end[1]} {things[1]} that {end[0]} {things[0]}.'
-    return [f'{str.rstrip()}.']
-
-print(recite(11, 11))
+    result = []
+    while start_verse <= end_verse:
+        build_str = ''
+        for i in range(start_verse, 1, -1):
+            build_str += f'{verses[i]} '
+        result.append(f'This is {f"{build_str}in " if start_verse in verses and end_verse in verses else ""}the house that Jack built.')
+        start_verse += 1
+    return result
